9618c090f7ca8d59661745a4886408d80b3460a2
jamesLaurino <petersthomas90@gmail.com>
2025-06-18 12:40:46 +0200

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  standalone:true
+})
+export class App {
+  protected title = 'angular-project';
+}
