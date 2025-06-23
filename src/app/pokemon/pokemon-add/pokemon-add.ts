0dc1a5f88c13c5ab5015a2c89888399c37ba5f48
jamesLaurino <petersthomas90@gmail.com>
2025-06-23 23:14:12 +0200

    Ajout du form et binding des valeurs sauf types

--- src/app/pokemon/pokemon-add/pokemon-add.ts
+++ src/app/pokemon/pokemon-add/pokemon-add.ts
@@ -1,7 +1,8 @@
 import {Component, inject} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {PokemonService} from '../../service/pokemon-service';
+import {POKEMON_RULES} from '../../model/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-add',
@@ -17,7 +18,28 @@ export class PokemonAdd {
 
   protected pokemonService = inject(PokemonService);
 
-  onSubmit() {
+  readonly form= new FormGroup({
+    name: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(POKEMON_RULES.MIN_NAME),
+        Validators.maxLength(POKEMON_RULES.MAX_NAME),
+        Validators.pattern(POKEMON_RULES.NAME_PATTERN)
+      ]
+    ),
+    live: new FormControl(''),
+    picture: new FormControl(''),
+    damage: new FormControl(''),
+    types: new FormArray(
+      [],
+      [
+        Validators.required,
+        Validators.maxLength(POKEMON_RULES.MAX_TYPES)
+      ]
+    )
+  });
 
+  onSubmit() {
+    console.log(this.form.value)
   }
 }
